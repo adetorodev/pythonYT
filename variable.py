2488382e1d5bdbcf187bb227534d627689edcd57
Ezekiel Adetoro <adetoroe787@gmail.com>
2025-03-14 20:13:58 +0100

    crash course

--- variable.py
+++ variable.py
@@ -5,63 +5,119 @@
 
 
 
-# 1️⃣ Installing Python & Setting Up VS Code/PyCharm ✅
-
 # 2️⃣ Python Syntax, Variables & Data Types
+# print("Hello, World!")
 
-name = "John"
-age = 45
-height = "4.5m"
+# def greetUser():
 
-# String
-# Integer
-# Float
-# Boolean
+name = "Alice" # String
+age = 25  #Integer
+height = 5.3   # Float
+is_student = True  # Boolean
 
-location = "Lagos" # String
+# print(age)
+# print(height)
+# print(is_student)
 
-age = 45 # Integer
-
-height = 4.5 # Float
-
-isAdmin = True # Boolean
-
-[1, 2, 4]  #List
-
-{1, 2, 4} # Set
-
-(1, 2, 3) #Tuple
-
-{"name" : "Alice"}
-
-
-
-
+# print(type(name))
+# print(type(age))
+# print(type(is_student))
 
 # 3️⃣ Conditional Statements (if, elif, else)
+# age = 17
+# if age >= 18:
+#     print("User is allowed to proceed")
+# elif age == 17:
+#     print("Wait one more year!")
+# else:
+#     print("You are not Allowed here")
 
 
 
 # 4️⃣ Loops (for, while)
+# for count in range(2, 15, 3):
+#     print(count)
+
+# count = -4
+# while count <= 5:
+#     print(count)
+#     count += 1
+
 
 
 
 # 5️⃣ Functions & Scope
+def greet(name): # def keyword, function name and a parameter
+    return f"Hello, {name}"  # return statement
+
+# Calling greet() function
+# print(greet("John Doe"))
+# print(greet("Alice Doe"))
+# print(greet("Smith Doe"))
+# print(greet("Jack Doe"))
 
 # Scope
 # Local Scope → Inside a function
+# def show():
+#     local_var = "I'm a local guy"
+#     print(local_var)
+
+# print(show())
+# print(local_var)
 # Global Scope → Outside functions
 
+global_var = "I'm a global guy men!"
+def show():
+    print(global_var)
 
+# print(show())
 
 # 6️⃣ Lists, Tuples, Sets, and Dictionaries
+# List ordered, mutable
+fruit = [  "Banana", "Apple", "Mango", "Cherry" ]
+# print("First List",fruit)
+fruit.append("Orange")
+# print("Added orange to List",fruit)
+sorted_list = fruit.sort()
+# print("Sorted List: ", fruit)
+
+# Tuple
+coordinate = (10, 20)
+# print(coordinate)
+# print(coordinate[0])
+# print(coordinate[1])
+
+# Set
+colors = {"red", "green", "blue"}
+colors.add("Yellow")
+# print(colors)
+
+# Dictionaries
+person = {"name": "Alice", "age": 25}
+# print(person["name"])
+# print(person["age"])
+# print(person)
 
 # 7️⃣ Exception Handling (try-except)
+# try:
+#     num = int(input("Enter a number: "))
+#     print(10 / num)
+
+# except ValueError:
+#     print("Please enter a number")
+# except ZeroDivisionError:
+#     print("Cannot divide by Zero")
+# finally:
+#     print("Execution Completed")
 
 
 
 # 8️⃣ Basic File Handling (open, read, write)
+with open("file.docx", "w") as file:
+    file.write("Hello World, Heelo World\n, Hello Worl")
 
-
+with open("file.docx", "r") as file:
+    content = file.read()
+    print(content)
 
 # 🛠 Project: To-Do List (CLI App)
